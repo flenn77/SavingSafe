8f6abd852c8b36eea4fb3522ba276daec2f76960
Steven <sivakaran19@hotmail.fr>
2023-08-31 14:53:32 +0200

    add Dropzone dependance

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
 ];
