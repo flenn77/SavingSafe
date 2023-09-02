82927020c1fc1deacd3239dbf79e0be5279ae91e
Steven Sivakaran <sivakaran19@hotmail.fr>
2023-09-02 16:35:42 +0200

    test

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,21 +4,42 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('first_name')
-            ->add('last_name')
+            ->add('first_name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le prénom ne doit pas être vide.',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                    ]),
+                ],
+            ])
+            ->add('last_name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom ne doit pas être vide.',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                    ]),
+                ],
+            ])
             ->add('phone_number')
             ->add('adress') // Notez que vous avez une petite faute d'orthographe. C'est 'adress' et non 'address'.
             ->add('city')
