52d516ed15068f8eaa08dac67db442eafe486e58
unknown <yanihaouili1@gmail.com>
2023-09-08 00:21:53 +0200

    deleting remove option for admin

--- public/js/login.js
+++ public/js/login.js
@@ -49,7 +49,7 @@ $.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles
           "line_linked": {
             "enable": true,
             "distance": 150,
-            "color": "#000000",
+            "color": "#ffffff",
             "opacity": 0.4,
             "width": 1
           },
